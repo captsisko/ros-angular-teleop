dcfad016efeeb0284d70e1daf61e13e84d5dd3c2
Ade 'sisko' Oduye <adeoduye@gmail.com>
2020-08-09 14:52:12 +0100

    Adds small package to serve as utility for the webapp. First function, re-orient the robot to an angle (which would be recieved from nipplejs) before a linear movement

---
+++ rosbridge_telegrams/src/rotate_bot.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Twist
+import math
+
+roll = pitch = yaw = 0
+target = 180 - 10
+kP = 1.0
+
+def getOdom(odom):
+    global roll, pitch, yaw
+    orientation = odom.pose.pose.orientation
+    orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
+    (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+
+rospy.init_node('rotate_bot')
+sub = rospy.Subscriber('/odom', Odometry, getOdom)
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+r = rospy.Rate(10)
+twist = Twist()
+
+while not rospy.is_shutdown():
+    # rotate_bot()
+
+    target_radians = target * math.pi/180
+    twist.angular.z = kP * (target_radians - yaw)
+    pub.publish(twist)
+
+
+    print("Target={}   Current={}".format(target_radians, yaw))
+    
+    if round(target_radians, 2) == round(yaw, 2):
+        exit()
+
+    r.sleep()
